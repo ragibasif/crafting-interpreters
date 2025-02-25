4435a2e5738dfb67710a2d8374a0caf24caaaf43
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2025-02-25 10:01:14 -0500

    {{UPDATE}} Tue Feb 25 10:01:14 2025

--- vm.c
+++ vm.c
@@ -28,6 +28,12 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b); \
+    } while (false)
 
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -47,16 +53,22 @@ static InterpretResult run() {
                                   push(constant);
                                   break;
                               }
+            case OP_ADD: BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE: BINARY_OP(/); break;
+            case OP_NEGATE: push(-pop()); break;
             case OP_RETURN: {
                                 printValue(pop());
                                 printf("\n");
                                 return INTERPRET_OK;
                             }
         }
+#endif
     }
 #undef READ_BYTE
 #undef READ_CONSTANT
-#endif
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
